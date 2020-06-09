d565cfc29d334cf1363f45239e144b7d80e56b46
Cancu <52618089+Cancuuu@users.noreply.github.com>
2020-06-09 19:21:06 -0300

    Create Kelvin Weather.js

---
+++ Kelvin Weather.js
@@ -0,0 +1,19 @@
+//Codecademy practice
+
+//kelvin tm
+const kelvin = 0;
+
+//kv a c
+const Celsius = kelvin - 273;
+
+//In the next line we pass C° to F
+fahrenheit = Celsius*(9/5) + 32;
+//Round down F
+fahrenheit = Math.floor(fahrenheit);
+//
+Newton = Celsius*(33/100);
+Newton = Math.floor(Newton);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+console.log(`The temperature is ${Newton} degrees Newton.`);
+
